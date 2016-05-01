0142fa956a17e3593693296871c57c96c1901392
Mohammad <momosharif58@gmail.com>
2016-04-30 21:46:44 -0400

    Gained access to the users education, need to use JSON to extract the school name.

--- Hang/app/src/main/java/mohammadsharif/com/hang/UserProfile.java
+++ Hang/app/src/main/java/mohammadsharif/com/hang/UserProfile.java
@@ -3,20 +3,40 @@ package mohammadsharif.com.hang;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.Profile;
+
+import org.json.JSONObject;
 
 public class UserProfile extends AppCompatActivity {
     private String name;
     private String school;
     private Profile user = Profile.getCurrentProfile();
     private TextView nameView;
-
+    private String asString;
     public UserProfile() {
-        name = user.getName();
-//        school = user.getProperty("")
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.e("JUNK", response.toString());
+                        asString = response.getRawResponse();
+
+                    }
+                });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "education");
+        request.setParameters(parameters);
+        request.executeAsync();
+
 
     }
 
